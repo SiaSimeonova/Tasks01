dd3af853bdcbbb33736f80cdbe12b308a82a38fc
Yordan <jordancho@mail.bg>
2015-12-12 12:39:28 +0200

    Version2 of TheHorseMoves

--- TheHorseMovesNew.java
+++ TheHorseMovesNew.java
@@ -1,28 +1,32 @@
 import java.util.Scanner;
 
 public class TheHorseMovesNew {
-	
-	static void horseMoves(char[][] a, int x, int y, int counter) {
+	static void horseMoves(char[][] a, int x, int y) {
 		if(x>a.length-1 || y>a[0].length-1 || x<0 || y<0){
 			return;
-		}
-		if(counter==a.length*a[0].length){
-			return;
-		}
+		}		
 		if(a[x][y]=='X'){
 			return;
-		}else{
-			a[x][y]='X';
-			counter++;
 		}
-		horseMoves(a, x-2, y-1, counter);
-		horseMoves(a, x-2, y+1, counter);
-		horseMoves(a, x-1, y-2, counter);
-		horseMoves(a, x-1, y+2, counter);
-		horseMoves(a, x+1, y-2, counter);
-		horseMoves(a, x+1, y+2, counter);
-		horseMoves(a, x+2, y-1, counter);
-		horseMoves(a, x+2, y+1, counter);
+			a[x][y]='X';			
+		
+		horseMoves(a, x-2, y-1);
+		horseMoves(a, x-2, y+1);
+		horseMoves(a, x-1, y-2);
+		horseMoves(a, x-1, y+2);
+		horseMoves(a, x+1, y-2);
+		horseMoves(a, x+1, y+2);
+		horseMoves(a, x+2, y-1);
+		horseMoves(a, x+2, y+1);
+	}
+	
+	static void printArray(char[][] a) {
+		for (int r = 0; r < a.length; r++) {
+			for (int c = 0; c < a[0].length; c++) {
+				System.out.print("| "+a[r][c]+" ");
+			}
+			System.out.println("|");
+		}
 	}
 
 	public static void main(String[] args) {
@@ -30,8 +34,7 @@ public class TheHorseMovesNew {
 		int rows = 0;
 		int cols = 0;
 		int horseRow = 0;
-		int horseCol = 0;
-		int counter=0;
+		int horseCol = 0;		
 		do {
 			System.out.println("Enter the number of the rows: ");
 			rows = sc.nextInt();
@@ -41,14 +44,11 @@ public class TheHorseMovesNew {
 			horseRow = sc.nextInt();
 			horseCol = sc.nextInt();
 		} while (rows < 0 && cols < 0 && horseRow < 0 && horseCol < 0 && horseRow >= rows && horseCol >= cols);
+		
 		char[][] horseMovesArray = new char[rows][cols];
-		horseMoves(horseMovesArray, horseRow, horseCol, counter);
-		for (int r = 0; r < horseMovesArray.length; r++) {
-			for (int c = 0; c < horseMovesArray[0].length; c++) {
-				System.out.print("| "+horseMovesArray[r][c]+" ");
-			}
-			System.out.println("|");
-		}
+		horseMoves(horseMovesArray, horseRow, horseCol);
+		printArray(horseMovesArray);
 		sc.close();
 	}
+
 }
