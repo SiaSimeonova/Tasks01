6d6f16614947284f6a9edd5c9b6a54505404245e
Yordan <jordancho@mail.bg>
2015-12-12 14:38:40 +0200

    Version2

--- TicTacToeNew.java
+++ TicTacToeNew.java
@@ -1,34 +1,25 @@
 import java.util.Scanner;
 
 public class TicTacToeNew {
-
-	static boolean move(char[][] a, int x, int y, char character, int counter) {
-		a[x][y] = character;
-		boolean isWinner = false;
+	static void printGameBoard(char[][] a) {
 		for (int row = 0; row < 3; row++) {
 			for (int col = 0; col < 3; col++) {
 				System.out.print("| " + a[row][col]);
 			}
 			System.out.println("|");
 		}
+	}
+
+	static boolean move(char[][] a, int row, int col, char character, int counter) {
+		a[row][col] = character;
+		boolean isWinner = false;
+		printGameBoard(a);
 		if (counter > 4) {
-			int countChar = 0;
-			for (int i = 0; i < 3; i++) {
-				if (a[i][y] == character) {
-					countChar++;
-				}
-				if (countChar == 3) {
-					isWinner = true;
-				}
+			if (a[row][0] == character && a[row][1] == character && a[row][2] == character) {
+				isWinner = true;
 			}
-			countChar = 0;
-			for (int i = 0; i < 3; i++) {
-				if (a[x][i] == character) {
-					countChar++;
-				}
-				if (countChar == 3) {
-					isWinner = true;
-				}
+			if (a[0][col] == character && a[1][col] == character && a[2][col] == character) {
+				isWinner = true;
 			}
 			if (a[0][0] == character && a[1][1] == character && a[2][2] == character) {
 				isWinner = true;
@@ -49,21 +40,26 @@ public class TicTacToeNew {
 		char character = ' ';
 		boolean winner = false;
 		while (counter < 9) {
-			if (!winner) {
-				do {
-					System.out.println("1: ");
-					row1 = sc.nextInt() - 1;
-					col1 = sc.nextInt() - 1;
-				} while (row1 > 2 || row1 < 0 || col1 > 2 || col1 < 0 || (ticTacToe[row1][col1] != '\u0000'));
-				counter++;
+			do {
 				if (counter % 2 == 0) {
-					character = 'O';
+					System.out.println("1: ");
 				}
 				if (counter % 2 != 0) {
-					character = 'X';
+					System.out.println("2: ");
 				}
-				winner = move(ticTacToe, row1, col1, character, counter);
-			} else {
+				row1 = sc.nextInt() - 1;
+				col1 = sc.nextInt() - 1;
+			} while (row1 > 2 || row1 < 0 || col1 > 2 || col1 < 0 || (ticTacToe[row1][col1] != '\u0000'));
+			counter++;
+			if (counter % 2 == 0) {
+				character = 'O';
+			}
+			if (counter % 2 != 0) {
+				character = 'X';
+			}
+			winner = move(ticTacToe, row1, col1, character, counter);
+
+			if (winner) {
 				if (character == 'X') {
 					System.out.println("1 is the winner!!!");
 					break;
@@ -72,7 +68,9 @@ public class TicTacToeNew {
 					System.out.println("2 is the winner!!!");
 					break;
 				}
+
 			}
+
 		}
 		if (!winner) {
 			System.out.println("It is a tie!");
